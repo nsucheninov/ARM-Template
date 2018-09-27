06c4c56c52e1f356073afc575a32ef65333e0f48
nsucheninov <nsucheninov@gmail.com>
2018-09-27 10:51:22 +0500

    removed Get-Credential

--- Invoke-Test.ps1
+++ Invoke-Test.ps1
@@ -1,8 +1,4 @@
-$cred = Get-Credential
-
-$params = @{ adminUsername = $cred.UserName; adminPassword = $cred.Password; dnsLabelPrefix = "nsuchen01" }
-
-$parameters = @{ ResourceGroupName = "test03"; TemplateFile = ".\azuredeploy.json"; Parameters = $params }
+$parameters = @{ ResourceGroupName = "test03"; TemplateFile = ".\azuredeploy.json"; TemplateParameterFile = ".\azuredeploy.parameters.json" }
 $script = @{ Path = ".\Template.Tests.ps1"; Parameters = $parameters }
 
 Invoke-Pester -Script $script
