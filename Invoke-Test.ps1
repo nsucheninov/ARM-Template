7cf786fb000e6ae08e83c10d95bf24004eedfe39
nickss2 <43530027+nickss2@users.noreply.github.com>
2018-09-27 15:25:35 +0500

    Update Invoke-Test.ps1

--- Invoke-Test.ps1
+++ Invoke-Test.ps1
@@ -2,9 +2,9 @@
 #$script = @{ Path = "$(System.DefaultWorkingDirectory)\Template.Tests.ps1"; Parameters = $parameters }
 
 Write-Host "Test:"
-Write-Host $(system.teamProject)
-Write-Host "----------------------------------"
-Write-Host $(system.DefaultWorkingDirectory)
+#Write-Host $(system.teamProject)
+#Write-Host "----------------------------------"
+Write-Host "source directory = $Env:BUILD_SOURCESDIRECTORY"
 Write-Host "----------------------------------"
 
 #Invoke-Pester -Script $script
