020c39ef27e5dd53fe6d3b8250cf60a91357309b
nickss2 <43530027+nickss2@users.noreply.github.com>
2018-09-27 15:37:53 +0500

    Update Invoke-Test.ps1

--- Invoke-Test.ps1
+++ Invoke-Test.ps1
@@ -3,8 +3,14 @@
 
 Write-Host "Test:"
 #Write-Host $(system.teamProject)
-#Write-Host "----------------------------------"
+Write-Host "----------------------------------"
 Write-Host "source directory = $Env:BUILD_SOURCESDIRECTORY"
 Write-Host "----------------------------------"
+(get-location).ToString()
+Write-Host "----------------------------------"
+Get-ChildItem | select Name
+Write-Host "----------------------------------"
+Get-ChildItem $Env:BUILD_SOURCESDIRECTORY
+
 
 #Invoke-Pester -Script $script
