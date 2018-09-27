23d0c28236d8f1b1d1b8b1f0289975074e9b4b49
nsucheninov <nsucheninov@gmail.com>
2018-09-27 12:32:32 +0500

    a

--- Invoke-Test.ps1
+++ Invoke-Test.ps1
@@ -1,4 +1,7 @@
 $parameters = @{ ResourceGroupName = "test03"; TemplateFile = ".\azuredeploy.json"; TemplateParameterFile = ".\azuredeploy.parameters.json" }
 $script = @{ Path = ".\Template.Tests.ps1"; Parameters = $parameters }
 
+Get-Location
+Get-ChildItem
+
 Invoke-Pester -Script $script
