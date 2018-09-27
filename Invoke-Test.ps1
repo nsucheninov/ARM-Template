f60ab83cfdcdadbee0c4d87e1fd69ab3f2d24888
nsucheninov <nsucheninov@gmail.com>
2018-09-27 17:53:09 +0500

    a

--- Invoke-Test.ps1
+++ Invoke-Test.ps1
@@ -1,4 +1,11 @@
 $parameters = @{ ResourceGroupName = "test03"; TemplateFile = "$Env:BUILD_SOURCESDIRECTORY\azuredeploy.json"; TemplateParameterFile = "$Env:BUILD_SOURCESDIRECTORY\azuredeploy.parameters.json" }
 $script = @{ Path = "$Env:BUILD_SOURCESDIRECTORY\Template.Tests.ps1"; Parameters = $parameters }
 
-Invoke-Pester -Script $script
+Common.TestResultsDirectory
+Build.BuildNumber
+
+$outputFile = "$ENV:COMMON_TESTRESULTSDIRECTORY\TEST-testFile-$ENV:BUILD_BUILDNUMBER.xml"
+
+Write-Host $outputFile
+
+Invoke-Pester -Script $script -OutputFile $outputFile
