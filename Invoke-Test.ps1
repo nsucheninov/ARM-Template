577b3a5aab59f632e6ed4f7258440f119de0da5a
nickss2 <43530027+nickss2@users.noreply.github.com>
2018-09-27 15:02:51 +0500

    Update Invoke-Test.ps1

--- Invoke-Test.ps1
+++ Invoke-Test.ps1
@@ -1,7 +1,11 @@
-$parameters = @{ ResourceGroupName = "test03"; TemplateFile = "$(System.DefaultWorkingDirectory)\azuredeploy.json"; TemplateParameterFile = "$(System.DefaultWorkingDirectory)\azuredeploy.parameters.json" }
-$script = @{ Path = "$(System.DefaultWorkingDirectory)\Template.Tests.ps1"; Parameters = $parameters }
+#$parameters = @{ ResourceGroupName = "test03"; TemplateFile = "$(System.DefaultWorkingDirectory)\azuredeploy.json"; TemplateParameterFile = "$(System.DefaultWorkingDirectory)\azuredeploy.parameters.json" }
+#$script = @{ Path = "$(System.DefaultWorkingDirectory)\Template.Tests.ps1"; Parameters = $parameters }
 
 Get-Location
 Get-ChildItem
 
-Invoke-Pester -Script $script
+Write-Host "Test:"
+Write-Host $System.DefaultWorkingDirectory
+Write-Host $system.teamProject
+
+#Invoke-Pester -Script $script
