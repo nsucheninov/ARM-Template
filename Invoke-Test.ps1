c06de91449d99a81e258b80ccd7e0b4aa2b5ba33
nsucheninov <nsucheninov@gmail.com>
2018-09-29 12:34:31 +0500

    Update Invoke-Test.ps1

--- Invoke-Test.ps1
+++ Invoke-Test.ps1
@@ -1,8 +1,6 @@
-$parameters = @{ ResourceGroupName = "test03"; TemplateFile = "$Env:BUILD_SOURCESDIRECTORY\azuredeploy.json"; TemplateParameterFile = "$Env:BUILD_SOURCESDIRECTORY\azuredeploy.parameters.json" }
+$parameters = @{ ResourceGroupName = "DevOps"; TemplateFile = "$Env:BUILD_SOURCESDIRECTORY\azuredeploy.json"; TemplateParameterFile = "$Env:BUILD_SOURCESDIRECTORY\azuredeploy.parameters.json" }
 $script = @{ Path = "$Env:BUILD_SOURCESDIRECTORY\Template.Tests.ps1"; Parameters = $parameters }
 
 $outputFile = "$ENV:COMMON_TESTRESULTSDIRECTORY\TEST-testFile-$ENV:BUILD_BUILDNUMBER.xml"
-
-Write-Host $outputFile
 
 Invoke-Pester -Script $script -OutputFile $outputFile -OutputFormat NUnitXml
